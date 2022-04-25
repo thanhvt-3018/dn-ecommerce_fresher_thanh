b0982d9e3527c73593a3095f1079747cabdee4b8
thanhvt-3018 <vo.trong.thanh@sun-asterisk.com>
2022-04-25 11:39:01 +0700

    Design home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+  rescue_from Pagy::OverflowError, with: :redirect_to_last_page
+
   before_action :set_locale
 
   private
@@ -8,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def redirect_to_last_page exception
+    redirect_to url_for(page: exception.pagy.last)
   end
 end
