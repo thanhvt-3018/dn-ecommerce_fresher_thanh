9bbd3bad1f77ff132968b93bad2ab51fbdaad9c1
thanhvt-3018 <vo.trong.thanh@sun-asterisk.com>
2022-04-26 11:16:05 +0700

    Add product to cart

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,16 @@
+class CartsController < ApplicationController
+  before_action :load_products_into_cart, only: :show
+
+  def show; end
+
+  def create
+    product_id = params.dig(:session, :product_id)
+    @cart.key?(product_id) ? @cart[product_id] += 1 : @cart[product_id] = 1
+    flash[:success] = t "global.success.add_cart"
+    redirect_to request.referer || root_path
+  end
+
+  def edit; end
+
+  def destroy; end
+end
