9acaa9c26643a07df1773ac5bc42779405e9c349
thanhvt-3018 <vo.trong.thanh@sun-asterisk.com>
2022-05-13 14:46:02 +0700

    Rspec - Unit test for helper

--- app/helpers/admin/orders/orders_helper.rb
+++ app/helpers/admin/orders/orders_helper.rb
@@ -2,9 +2,12 @@ module Admin::Orders::OrdersHelper
   def contain_status order
     array_status = []
     t("order_status").to_a.each{|status| array_status.push(status.reverse)}
-    @arr_status = array_status if order.pending?
-    @arr_status = array_status.drop 1 if order.confirmed?
-    @arr_status = array_status.drop 2 if order.delivering?
-    @arr_status = array_status.drop 3 if order.received?
+    return array_status if order.pending?
+
+    return array_status.drop 1 if order.confirmed?
+
+    return array_status.drop 2 if order.delivering?
+
+    return array_status.drop 3 if order.received?
   end
 end
