1ca0d5933cf39edcf92e16198f4d72becfeaa40e
thanhvt-3018 <vo.trong.thanh@sun-asterisk.com>
2022-05-16 13:58:51 +0700

    Auth with devise gem

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  before_action :store_location
   before_action :load_products_into_cart, :total_price_into_cart, only: :show
   before_action :handle_quantity, only: :update
 
